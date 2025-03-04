745c59e36a15fa6b3f47da2c4e0ab9e4315e09c7
ToniGimenez <tonigimenez555@gmail.com>
2025-03-04 16:34:39 +0100

    feat: TablonView with bottomBar & SearchBar

--- composeApp/src/commonMain/kotlin/com/connectyourcoach/connectyourcoach/App.kt
+++ composeApp/src/commonMain/kotlin/com/connectyourcoach/connectyourcoach/App.kt
@@ -3,12 +3,13 @@ package com.connectyourcoach.connectyourcoach
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import com.connectyourcoach.connectyourcoach.views.LoginView
+import com.connectyourcoach.connectyourcoach.views.TablonView
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        LoginView()
+        TablonView()
     }
 }
