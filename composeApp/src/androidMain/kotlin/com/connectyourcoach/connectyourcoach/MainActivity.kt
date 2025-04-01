128dc91f4f5ba30b10b02f2b0e52a0669a89daef
AuraTurqesa <157357938+AuraTurqesa@users.noreply.github.com>
2025-04-01 11:17:55 +0200

    feat: Implementació de la API de imatges de perfil a AndroidMain
    Falta:
    
    - Implementar API a DesktopMain
    - Implementar API a IOSMain
    - Modificar build.gradle.kts i libs.versions.toml per fer funcionar totes les funcionalitats
    - Correcció d'errors

--- composeApp/src/androidMain/kotlin/com/connectyourcoach/connectyourcoach/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/connectyourcoach/connectyourcoach/MainActivity.kt
@@ -3,23 +3,29 @@ package com.connectyourcoach.connectyourcoach
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.connectyourcoach.connectyourcoach.views.TablonView
+import androidx.compose.ui.Alignment
+import io.ktor.client.HttpClient
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        startKoin {
+            androidContext(this@MainActivity)
+            modules(appModule)
+        }
+
         setContent {
-            App()
+            // Obtenim el client HTTP directament (sense try-catch)
+            val httpClient = org.koin.compose.koinInject<HttpClient>()
+            App(httpClient = httpClient)
         }
     }
 }
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
+val appModule = module {
+    single { createHttpClient() }
 }
